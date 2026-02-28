49485ef585d08ec4c71dcdba640e746412faa17b
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-28 16:52:28 +0530

    form

--- form/FormDemo1.js
+++ form/FormDemo1.js
@@ -1,5 +1,5 @@
 const submitHandler = (event)=>{
-    
+
     event.preventDefault();
     console.log("event...",event)
     const name = document.getElementById("name") //<input>
@@ -14,5 +14,35 @@ const submitHandler = (event)=>{
         nameValue.innerHTML = name.value
         nameValue.style.color="black"
     }
+
+
+    const gender = document.getElementsByName("gender") //[][]
+    //console.log(gender)
+    // if(gender[0].checked==true){
+    //     console.log(gender[0].value)
+    // }
+    // else if(gender[1].checked==true){
+    //     console.log(gender[1].value)
+    // }
+
+    for(let i=0;i<gender.length;i++){
+        if(gender[i].checked==true){
+            console.log(gender[i].value)
+        }
+    }
+
+    const hobbies = document.getElementsByName("hobbies")
+    ///console.log(hobbies)
+    for(let i=0;i<hobbies.length;i++){
+        if(hobbies[i].checked==true){
+            console.log(hobbies[i].value)
+        }
+    }
+
+    const country = document.getElementById("country")
+    console.log(country.value)
+
+
+
 
 }
