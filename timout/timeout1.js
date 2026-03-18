b445c05022b6274afaa1deb6dd9b5c66b8bb47a1
samirvithlani <samir.vithlani83955@gmail.com>
2026-03-18 18:18:38 +0530

    timeput

---
+++ timout/timeout1.js
@@ -0,0 +1,37 @@
+console.log("file loaded")
+
+const callAfter3 = ()=>{
+    console.log("function called after 3 sec..")
+}
+
+//callAfter3()
+setTimeout(()=>{
+    //callAfter3()
+},3000) //mill seconds
+
+// setInterval(() => {
+//     console.log("function called...")
+// }, 1000);
+
+//clear
+// var count =1;
+// var id = setInterval(()=>{
+//     console.log("count....",count)
+//     count++
+//     //clearInterval(id)
+//     if(count>5){
+//         clearInterval(id)
+//     }
+
+// },1000)
+
+
+var count =1;
+var id = setInterval(()=>{
+    
+    console.log("count =",count)
+    count++
+    setTimeout(()=>{
+        clearInterval(id)
+    },4000)
+})
