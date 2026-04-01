d1e65bd81b99a9a2536b46860a75e421be42a374
samirvithlani <samir.vithlani83955@gmail.com>
2026-04-01 18:22:14 +0530

    date

---
+++ date/DateDemo1.js
@@ -0,0 +1,30 @@
+var date = new Date()
+console.log(date)
+
+//multipule const date class have
+
+var date2 = new Date(2026,11,25,10,30,45,123)
+console.log(date2)
+
+var date3 = new Date(2026,11,25)
+console.log(date3)
+
+var date4 = new Date(2026,11)
+console.log(date4)
+
+var date5 = new Date(1) //1 mils
+console.log(date5)
+
+var date6 = new Date()
+console.log(date6)
+
+//string
+var date7 = new Date("2026-11-25T10:30:45.123Z")
+console.log("date7",date7)
+
+var date8 = new Date("2026-11-25")
+console.log("date8",date8)
+
+var date9 = new Date("2026-11")
+console.log("date9",date9)
+
